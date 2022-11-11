5e40900504717cb70d400e2d8a56d98c0ddbd0bd
DanielZanad <daniel.zanad.dz@gmail.com>
2022-11-11 14:09:25 -0300

    add: navbar and footer

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p>ReactGram &copy; 2022</p>
+    </footer>
+  )
+}
+
+export default Footer;
