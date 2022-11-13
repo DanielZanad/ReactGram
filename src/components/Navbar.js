9db4dd18a2158f8215b5d3d5ae078db7a49fe49e
DanielZanad <daniel.zanad.dz@gmail.com>
2022-11-13 17:19:55 -0300

    add: register page and global css form completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav id="nav">
       <Link to="/">ReactGram</Link>
-      <form>
+      <form id="search-form">
         <BsSearch />
         <input type="text" placeholder="Pesquisar" />
       </form>
