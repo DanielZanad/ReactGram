4ae147ae0ed69760d9ed0b585450480508cc7943
DanielZanad <daniel.zanad.dz@gmail.com>
2022-12-05 14:27:27 -0300

    add: LikeContainer component

---
+++ src/components/LikeContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import "./LikeContainer.css";
+
+import {BsHeart, BsHeartFill} from "react-icons/bs";
+
+const LikeContainer = ({photo, user, handleLike}) => {
+  return (
+    <div className="like">
+      {photo.likes && user && (
+        <>
+          {photo.likes.includes(user._id) ? (
+            <BsHeartFill />
+          ) : (
+            <BsHeart onClick={() => handleLike(photo)} />
+          )}
+          <p>{photo.likes.length} likes(s)</p>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default LikeContainer
